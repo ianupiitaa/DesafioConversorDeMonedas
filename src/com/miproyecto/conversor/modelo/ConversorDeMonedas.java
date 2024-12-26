15c0d2565802862c6361271b0c252cc9610d6603
ianupiitaa <ianyofes73@gmail.com>
2024-12-25 23:44:37 -0600

    solicitud de moneda y cracion de objeto conversor de monedas

---
+++ src/com/miproyecto/conversor/modelo/ConversorDeMonedas.java
@@ -0,0 +1,4 @@
+package com.miproyecto.conversor.modelo;
+
+public class ConversorDeMonedas {
+}
--- src/com/miproyecto/conversor/principal/Principal.java
+++ src/com/miproyecto/conversor/principal/Principal.java
@@ -1,7 +1,16 @@
 package com.miproyecto.conversor.principal;
 
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
-        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("********Conversor de monedas********");
+        System.out.println("Selecciona el tipo de moneda que deseas usar");
+        var monedaUso = teclado.nextLine();
+        System.out.println("Selecciona el tipo de moneda que desea obtener de cambio");
+        var monedaCambio = teclado.nextLine();
+//        System.out.println(monedaUso + "*****" +monedaCambio);
+
     }
 }
